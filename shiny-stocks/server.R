cf5d90299840c1d50f76e25d12fb8ecd31c72b44
Pascal2893 <pascals@sfu.ca>
2020-04-04 17:35:53 -0700

    - better comments - file input and data loading seperately - conditional panels and new layout

--- shiny-stocks/server.R
+++ shiny-stocks/server.R
@@ -1,13 +1,5 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinythemes)
 library(ggthemes)
 library(tidyverse)
 library(janitor)
@@ -15,7 +7,8 @@ library(BatchGetSymbols)
 
 server <- function(input, output, session) {
   
-  data <- reactive({
+  ##### get path to data #####  
+  input_file <- reactive({
     
     req(input$file)
     req(input$Dates)
@@ -27,20 +20,7 @@ server <- function(input, output, session) {
     if(file_extension == "csv") {
       
       # pull data into shiny application
-      (BatchGetSymbols(tickers = read.csv(input$file$datapath) %>%
-                         dplyr::pull(),
-                       first.date = input$Dates[1],
-                       last.date = input$Dates[2],
-                       freq.data = input$time,
-                       cache.folder = file.path(tempdir(),
-                                                'BGS_Cache'))  %>%
-         .[[2]] %>%
-         dplyr::ungroup() %>%
-         dplyr::select(price.open, price.high, price.low, price.close,
-                       volume, price.adjusted, ref.date, ticker) %>%
-         dplyr::mutate(ref.date = as.character(ref.date)) %>%
-         janitor::clean_names())
-      
+      data_file <- input$file
       
     } else {
       
@@ -48,12 +28,37 @@ server <- function(input, output, session) {
       
     }
     
+    return(data_file)
+    
+  })
+  # ---------------------------------------------------------------------------------
+  
+  ##### get data #####
+  data <- reactive({
+    
+    req(input$file)
+    req(input$Dates)
+    req(input$time)
+    
+    # pull data into shiny application
+    (BatchGetSymbols(tickers = read.csv(input_file()$datapath) %>%
+                       dplyr::pull(),
+                     first.date = input$Dates[1],
+                     last.date = input$Dates[2],
+                     freq.data = input$time,
+                     cache.folder = file.path(tempdir(),
+                                              'BGS_Cache'))  %>%
+        .[[2]] %>%
+        dplyr::ungroup() %>%
+        dplyr::select(price.open, price.high, price.low, price.close,
+                      volume, price.adjusted, ref.date, ticker) %>%
+        dplyr::mutate(ref.date = as.character(ref.date)) %>%
+        janitor::clean_names())
+    
   })
   
   
-  # Table of stocks displayed
-  # Table is reactive and changes depending on the selected stocks
-  # Table is made reaktive through the output$row_stocks, renderUI function
+  ##### output table #####
   output$stock_data_table <- renderTable({
     if(is.null(data())){return ()}
     
@@ -66,33 +71,62 @@ server <- function(input, output, session) {
     }
     
   })
+  # ------------------------------------------------------------------------
   
-  output$stock_data_plot <- renderPlot({
+  ###### outputs line chart of stocks #####
+  output$stock_data_plot <- renderPlotly({
     if(is.null(data())){return ()}
     
     selection <- paste0(input$stocks_selected, collapse = "|")
-    if(length(input$stocks_selected != 0)) {
+    data() %>%
+      dplyr::filter(stringr::str_detect(ticker, pattern = selection)) %>%
+      dplyr::mutate(ref_date = lubridate::ymd(ref_date)) -> df
+    
+    if(length(input$stocks_selected != 0) & !(input$smooth)) {
       
-      data() %>%
-        dplyr::filter(stringr::str_detect(ticker, pattern = selection)) -> df
+      plotly::ggplotly(
+        
+        ggplot(df, aes_string(x = "ref_date", y = input$y, 
+                              col = "ticker", group = "ticker")) +
+          geom_point() +
+          geom_line() +
+          xlab("") +
+          ylab(input$y) +
+          theme_wsj() +
+          ggtitle("Stock Development Over Time") 
+      )
+      
+    } else if (length(input$stocks_selected) != 0 & input$smooth){
+      
+      plotly::ggplotly(
+        
+        ggplot(df, aes_string(x = "ref_date", y = input$y, 
+                              col = "ticker", group = "ticker")) +
+          geom_point() +
+          geom_line() +
+          geom_smooth(se = F) +
+          xlab("") +
+          ylab(input$y) +
+          theme_tufte() +
+          ggtitle("Stock Development Over Time") 
+      )
+      
+    } else {
+      
+      ggplot() +
+        ggtitle("No Stock Selected") +
+        theme_solid() +
+        theme(plot.title = element_text(size = 20, hjust = 0.5))
       
     }
     
-    ggplot(df, aes_string(x = "ref_date", y = input$y, 
-                          col = "ticker", group = "ticker")) +
-      geom_point() +
-      geom_line() +
-      geom_smooth(se = F) +
-      xlab("Date") +
-      ylab(input$y) +
-      theme_economist() +
-      ggtitle("Stock Development Over Time") 
     
     
   })
+  # -----------------------------------------------------------------
   
   
-  
+  ##### based on data input, get all tickers as character vector #####
   different_stocks <- reactive({
     if(is.null(data())){return ()}
     
@@ -103,7 +137,9 @@ server <- function(input, output, session) {
     
     
   })
+  # -------------------------------------------------------------------
   
+  ##### based on tickers, create a dropdown menu with imported ticker data #####
   output$row_stocks <- renderUI({
     if(is.null(data())){return ()}
     
@@ -112,25 +148,42 @@ server <- function(input, output, session) {
                        choices = different_stocks(), multiple = TRUE)
     
   })
+  # ----------------------------------------------------------------------------
   
+  ##### based on column names of data, add drop down menue to select column for plotting #####
   output$column_names <- renderUI({
     if(is.null(data())){return ()}
     
-    shiny::selectInput("y", "Columns:", 
-                       choices = names(data()))
+    col_names <- data() %>%
+      dplyr::select(price_open, price_high, price_low, price_close,
+                    volume, price_adjusted) %>%
+      colnames()
     
+    shiny::selectInput("y", "Columns:",
+                       choices = col_names)
+
   })
+  # ------------------------------------------------------------------------------------------
   
-  
+  ##### after data import, create two tabs #####
   output$contents <- renderUI({
     if(is.null(data())){return ()}
     
+    tabsetPanel(id = "panel", 
+      tabPanel("Stocks", tableOutput("stock_data_table")),
+      tabPanel("Plots", value = "tab_2", plotOutput("stock_data_plot")))
     
-    tabsetPanel(tabPanel("Stocks", value = 1, tableOutput("stock_data_table")),
-                tabPanel("Plots", value = 2, plotOutput("stock_data_plot")))
+  })
+  # --------------------------------------------
+  
+  
+  output$panel_2 <- renderUI({
+    if(is.null(data())){return (print("No data available"))}
     
-    
+    tabsetPanel(tabPanel("Plots", plotlyOutput("stock_data_plot")))
     
   })
   
 }
+
+
--- shiny-stocks/ui.R
+++ shiny-stocks/ui.R
@@ -1,45 +1,79 @@
 library(shiny)
+library(shinythemes)
 
 
 ui <- fluidPage(
   
-  # App title ----
-  titlePanel("Uploading Files"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Select a file
-      shiny::fileInput("file", "Choose CSV File"),
-      
-      # Select Date Range
-      shiny::dateRangeInput("Dates",
-                            "Dates:",
-                            start = "2020-04-01",
-                            end = Sys.Date(),
-                            min = "2010-01-01",
-                            max = Sys.Date(),
-                            format = "yyyy/mm/dd",
-                            separator = "-"),
-      
-      shiny::radioButtons("time", "Choose Frequency", 
-                          choices = c('daily', 'weekly', 'monthly', 'yearly')),
-      
-      shiny::uiOutput("row_stocks"),
-      shiny::uiOutput("column_names")
-      
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      
-      shiny::uiOutput("contents")
-      
-    )
-    
+  shiny::navbarPage(theme = shinytheme("sandstone"), collapsible = TRUE,
+                    title = "Stock Analysis",
+                    
+                    
+                    # Start of tab panel #1 -------------------------------------------------------------------------------                    
+                    shiny::tabPanel("Data Collection",
+                                    
+                                    # Sidebar layout with input and output definitions ----
+                                    sidebarLayout(
+                                      
+                                      # Sidebar panel for inputs ----
+                                      sidebarPanel(
+                                        
+                                        # Input: Select a file
+                                        shiny::fileInput("file", "Choose CSV File"),
+                                        
+                                        
+                                        # Select Date Range
+                                        shiny::dateRangeInput("Dates",
+                                                              "Dates:",
+                                                              start = "2020-04-01",
+                                                              end = Sys.Date(),
+                                                              min = "2010-01-01",
+                                                              max = Sys.Date(),
+                                                              format = "yyyy/mm/dd",
+                                                              separator = "-"),
+                                        
+                                        shiny::radioButtons("time", "Choose Frequency:", 
+                                                            choices = c('daily', 'weekly', 'monthly', 'yearly')),
+                                        
+                                        shiny::uiOutput("row_stocks"),
+                                        
+                                        shiny::conditionalPanel(condition = "input.panel == 'tab_2'",
+                                                                shiny::uiOutput("column_names"),
+                                                                checkboxInput("smooth", "Add Smooth Line:"))
+                                        
+                                      ),
+                                      
+                                      # Main panel for displaying outputs ----
+                                      mainPanel(
+                                        
+                                        shiny::uiOutput("contents")
+                                        
+                                      ))
+                                    
+                    ),
+                    # End of tab panel # 1 ------------------------------------------------------------------------------------------
+                    
+                    # Start of tab panel #2 -----------------------------------------------------------------------------------------
+                    shiny::tabPanel("Data Exploration",
+                                    
+                                    # Sidebar layout with input and output definitions ----
+                                    sidebarLayout(
+                                      
+                                      # Sidebar panel for inputs ----
+                                      sidebarPanel(
+                                        
+                                       
+                                        
+                                      ),
+                                      
+                                      # Main panel for displaying outputs ----
+                                      mainPanel(
+                                        
+                                        
+                                        #shiny::uiOutput("contents")
+                                        
+                                      ))
+                                    
+                    )
+                    
   )
 )
